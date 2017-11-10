962832c0a2b83ec0ab06978debba6d107444e537
Kyle <kyle.henwood@gmail.com>
2017-11-10 16:39:46 +1300

    updates

---
+++ _shnappy-website/scss/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'scss/public.css' : 'scss/public.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		},
+    autoprefixer:{
+      dist:{
+        files:{
+          'scss/public.css' : 'scss/public.css'
+        }
+      }
+    }
+	});
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.registerTask('default',['watch']);
+  grunt.registerTask('dev',['watch','sass','autoprefixer']);
+}
